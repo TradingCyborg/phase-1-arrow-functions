26ff959abf841f1bb1c0ea7289fd500080bc0314
Daniel Kinyua <my.github.kinyuad29@gmail.com>
2023-09-23 05:28:12 +0300

    I have defined denominator but still bringing assertion error

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide = (numerator, denominator) => {
+    if( denominato ===0) {
+        throw new Error('Cannot divide by zero');
+
+    }
+    return numerator/denominator;
+    };
+    function divide() {
+        const numerator = 2000;
+        const denominator = 100;
+        const result = numerator/denominator;
+        return result;
+    }
+    const divisionResult = divide();
+    console.log(divisionResult);
+    const square = (number) => number * number;
+    const add = (a,b) => {
+        return a + b;
+    }
